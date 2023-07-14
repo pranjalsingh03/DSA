50f43a319a45756731c1b6cb4dd3957001aecdf6
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-07-14 20:10:07 +0530

    init

--- Arrays/baisc.cpp
+++ Arrays/baisc.cpp
@@ -1,2 +1,12 @@
-#include<isostream>
+#include<iostream>
 
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int *ptr = arr;
+    std::cout << *ptr << std::endl;
+    std::cout << *(ptr+1) << std::endl;
+    std::cout << *(ptr+2) << std::endl;
+    std::cout << *(ptr+3) << std::endl;
+    std::cout << *(ptr+4) << std::endl;
+    return 0;
+}
