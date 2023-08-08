0a70be1f2f842cc6472f65dadde8aed2b7d5c21f
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-08 11:16:13 +0530

    basic of functins

---
+++ Function/baisc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int myfunc(int a){
+    cout<<"the value of a is:"<<a<<endl;
+    return a*a;
+}
+
+int main(){
+    int ab=10;
+    cout<<ab<<endl;
+    int b;
+    cout<<"Enter the value of b:"<<b;
+    cin>>b;
+    cout<<"The value of b is: "<<b<<endl;
+    cout<<myfunc(12)<<endl;
+}
+
