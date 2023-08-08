dfaa577142aabe2ee43d6647e4c239bdb6f08050
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-08 11:11:28 +0530

    class

--- Arrays/baisc.cpp
+++ Arrays/baisc.cpp
@@ -3,6 +3,7 @@
 int main(){
     int arr[5] = {1,2,3,4,5};
     int *ptr = arr;
+    
     std::cout << *ptr << std::endl;
     std::cout << *(ptr+1) << std::endl;
     std::cout << *(ptr+2) << std::endl;
