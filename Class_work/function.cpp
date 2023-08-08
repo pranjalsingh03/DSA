9b154fe6451132df709af2787aa83d0c16d6c1aa
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-08 11:16:03 +0530

    function start

--- Class_work/function.cpp
+++ Class_work/function.cpp
@@ -5,6 +5,11 @@ int myfunc(int a){
     cout<<"the value of a is:"<<a<<endl;
     return a*a;
 }
+int mySum(int a , int b){
+    cout<<"The sum of two numbers are :"<<a+b<<endl;
+    
+}
+
 int main(){
     int ab=10;
     cout<<ab<<endl;
@@ -13,6 +18,6 @@ int main(){
     cin>>b;
     cout<<"The value of b is: "<<b<<endl;
     cout<<myfunc(12)<<endl;
-
+    cout<<mySum(2,3)<<endl;
 }
 
