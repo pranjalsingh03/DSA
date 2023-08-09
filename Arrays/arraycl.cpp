8832659feefaf64d2b6e03a0a82bbf42cf08068c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-09 21:37:06 +0530

    array in class

--- Arrays/arraycl.cpp
+++ Arrays/arraycl.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
-
 using namespace std;
-
 int main(){
-    int n, arr[n] = {1,2,3,4,5};
-    for(arr[0]=0;arr[n]<6;n++){
-        cout<<arr[n]<<endl;
+    int i,n,k,l,item,j, arr[10];
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>arr[i];
     }
-
-}
+    cout<<"Enter the position where you want to insert the value:";
+    cin>>k;
+    cout<<"Enter the value you want to insert:";
+    cin>>item;
+    for(j=n-1;j>=k;j--){
+        arr[j+1]=arr[j];
+    }
+    arr[k]=item;
+    n=n+1;
+    for(l=0;l<n;l++){
+        cout<<arr[l]<<endl;
+    }
+    return 0;
+} 
