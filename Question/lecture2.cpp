d94296f7205f112475485389983c54c6c0bba6e3
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-10 07:54:09 +0530

    question on neocolab

---
+++ Question/lecture2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;    
+
+int main(){
+    string name , password;
+    cin>> name >>password;
+    cout<<"Name: "<<name<<endl ;
+    cout<<"Password: "<<password<<endl;
+}
