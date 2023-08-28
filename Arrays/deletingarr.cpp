2414b6606973fb167a0543ab4b8a6b0789c826be
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-08-28 10:51:46 +0530

    deleting the array element

--- Arrays/deletingarr.cpp
+++ Arrays/deletingarr.cpp
@@ -3,6 +3,32 @@
 #include<iostream>
 using namespace std;
 
+void Toprint(int NA[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << NA[i] <<",";
+    }
+    
+};
+
+int search(int NA[]){
+    int n;
+    cout<<"Enter the number you want to search:";
+    cin>>n;
+    for (int i = 0; i < 10; i++)
+    {
+        if (NA[i]==n)
+        {
+            cout<<"Number found at position "<<i;
+            return 0;
+        }
+        
+    }
+    cout<<"Number not found";
+    return 0;
+}
+
+
 int main(){
     int i,l,j,k,n,x,y, NA[10];
     cout<<"Enter the no of elements:";
@@ -10,6 +36,7 @@ int main(){
     for(i=0;i<n;i++){
         cin>>NA[i];
     }
+    Toprint(NA,n);
     cout<<"Enter the palce where you want to delete:";
     cin>>k;
     for(i=k+1;i<n;i++){
@@ -19,4 +46,5 @@ int main(){
     for(l=0;l<n;l++){
         cout<<NA[l]<<",";
     }
+    search(NA);
 }
