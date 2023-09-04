ce94771167cf6829ad555fb50fe8dcbdf653b82c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-09-04 11:17:16 +0530

    pointer

--- Class_work/pointer.cpp
+++ Class_work/pointer.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
+class Test{
+    int a =10;
+    public:
+    Test(){
+        cout<<"Object created for Test"<<endl;
+    }
+    void show(){
+        cout<<"Value of a :"<<endl;
+    }
+};
+
+struct Student{
+    int x=10;
+};
+
 int main(){
-    int myNum=10;
-    int *ptr;
-    ptr=&myNum;
-    cout<<"Address of myNum "<<ptr;
-    cout<<"\nValue of myNum "<<*ptr;
+    // Test t;
+    // t.show();
+    struct Student *s;
+    Student s1;
+    s=&s1;
+    cout<<s->x<<endl;
+    Test *tp;
+    Test t;
+    tp=&t;
+    tp->show();
+    t.show();
 }
