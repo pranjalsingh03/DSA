b17a6a299804e06675de0d7ea546196cf4889aaf
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-09-04 11:17:40 +0530

    linked list

---
+++ Linked List/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+struct linked_list{
+    int data ;
+    struct linked_list *next;
+    };
+typedef struct linked_list node:
+node*head;
+head = (node*)malloc(size of (node)):
+head->data=10;
+head->next=NULL;
