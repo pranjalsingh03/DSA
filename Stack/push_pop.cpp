b8a682981283fa14d604f8a41f320c10ee51e784
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-09-11 18:52:50 +0530

    push pop in stack

---
+++ Stack/push_pop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void push(int c,int a){
+    int i,c;
+    for(int i=0;i<c;i++){
+        cin>>a[i]
+    }
+    for(int j=0;j<c;j++){
+        cout<<a[j]<<endl;
+    }
+}
+
+int main(){
+    int  c;
+    // cin>>c;
+    int a[5];
+    push(c,a[5]);
+    return 0;
+
+}
