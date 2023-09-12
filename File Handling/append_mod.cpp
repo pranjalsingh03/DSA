2b839f32777a30071379f7b16065c14530bd83c6
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-09-12 15:35:42 +0530

    append in file

--- File Handling/append_mod.cpp
+++ File Handling/append_mod.cpp
@@ -5,13 +5,27 @@ using namespace std;
 int main(){
     char s[30];
     ofstream fileout;
-    ifstream filein;
-    fileout.open("data.txt",ios::app);
-    fileout<<"India\n";
-    fileout<<"USA\n";
-    fileout<<"UK\n";
+    fstream filein;
+    // fileout.open("data.txt",ios::app);
+    // fileout<<"India\n";
+    // fileout<<"USA\n";
+    // fileout<<"UK\n";
+    // fileout.close();
+    // filein.open("data.txt");
+    // filein.getline(s,30);
+    // cout<<s;
+
+    fileout.open("hello.txt",ios::out);
+    fileout<<"Hello to the programming world";
     fileout.close();
-    filein.open("data.txt");
-    filein.getline(s,30);
-    cout<<s;
+    filein.open("hello.txt" ,ios::in|ios::out);
+    
+    cout<<filein.tellg()<<endl;
+    cout<<filein.tellp()<<endl;
+    filein.seekp(9);
+    filein.seekg(12);
+    cout<<filein.tellg()<<endl;
+    cout<<filein.tellp()<<endl;
+    filein>>s;
+    cout<<s<<endl;
 }
