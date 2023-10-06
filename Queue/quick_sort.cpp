e342d3a99c3e5c5bba5cac1ce441bfc67ee4cdac
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-10-06 15:30:07 +0530

    quick sort

--- Queue/quick_sort.cpp
+++ Queue/quick_sort.cpp
@@ -3,20 +3,45 @@
 
 using namespace std;
 
-int partition(vector<int>& arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
+// int partition(vector<int>& arr, int low, int high) {
+//     int pivot = arr[high];
+//     int i = low - 1;
+//     for (int j = low; j < high; j++) {
+//         if (arr[j] < pivot) {
+//             i++;
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+//     swap(arr[i+1], arr[high]);
+//     return i+1;
+// }
+
+int partition(int arr[],int low,int high){
+    int pivot=arr[low];
+    int i=low+1;
+    int j=high;
+    while (true)
+    {
+        while (arr[i]<=pivot)
+        {
             i++;
-            swap(arr[i], arr[j]);
+        }a
+        while (arr[j]>pivot)
+        {
+            j--;
+        }
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
+        else{
+            swap(arr[low],arr[j]);
+            return j;
         }
     }
-    swap(arr[i+1], arr[high]);
-    return i+1;
+    
 }
 
-void quickSort(vector<int>& arr, int low, int high) {
+void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi-1);
@@ -25,8 +50,8 @@ void quickSort(vector<int>& arr, int low, int high) {
 }
 
 int main() {
-    vector<int> arr = {10, 7, 8, 9, 1, 5};
-    int n = arr.size();
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
     quickSort(arr, 0, n-1);
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
