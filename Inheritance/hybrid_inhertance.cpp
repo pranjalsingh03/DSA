d29fa7e979991949bfe153d3f2bdce4ba550e24a
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-10-13 23:39:04 +0530

    hybrid inhertance

--- Inheritance/hybrid_inhertance.cpp
+++ Inheritance/hybrid_inhertance.cpp
@@ -1,50 +1,59 @@
-#include <iostream>
-using namespace std;
+*#include<iostream> using namespace std;
 
-class Student {
-   protected:
+class Student
+{
+protected:
     int roll_no;
 
-   public:
-    void set_roll_no(int r) {
+public:
+    void set_roll_no(int r)
+    {
         roll_no = r;
     }
 };
 
-class Test : virtual public Student {
-   protected:
+class Test : virtual public Student
+{
+protected:
     float sub1;
     float sub2;
 
-   public:
-    void set_marks(float s1, float s2) {
+public:
+    void set_marks(float s1, float s2)
+    {
         sub1 = s1;
         sub2 = s2;
+        /
     }
 };
 
-class Sports : virtual public Student {
-   protected:
+class Sports : virtual public Student
+{
+protected:
     float score;
 
-   public:
-    void set_score(float sc) {
+public:
+    void set_score(float sc)
+    {
         score = sc;
     }
 };
 
-class Result : public Test, public Sports {
-   float total;
+class Result : public Test, public Sports
+{
+    float total;
 
-   public:
-    void display() {
+public:
+    void display()
+    {
         total = sub1 + sub2 + score;
         cout << "Roll No: " << roll_no << endl;
         cout << "Total Score: " << total << endl;
     }
 };
 
-int main() {
+int main()
+{
     Result r;
     int roll_no;
     float sub1, sub2, score;
