fd420323f60e6d84773545bf22f47a0199531c16
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-10-31 10:27:40 +0530

    heap sort using maxheap

---
+++ Heap/heapsort.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+void maxHeapify(int arr[],int n,int i){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n && arr[left]> arr[largest]){
+        largest=left;
+    }
+    if(right<n && arr[right]> arr[largest]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        maxHeapify(arr,n,largest);
+    }
+}
+
+void buildheap(int arr[],int n){
+    for(int i=n/2 -1 ; i>=0 ; i--){
+        maxHeapify(arr,n,i);
+    }
+}
+
+void heapsort(int arr[],int n){
+    buildheap(arr,n);
+    for(int i= n-1 ; i>0;i--){
+        swap(arr[0],arr[i]);
+        maxHeapify(arr,i,0);
+    }
+}
+
+int main(){
+    int n;
+    std::cout << "enter the number of element" << std::endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"original arrray"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    heapsort(arr,n);
+    cout<<"sorted array"<<endl;
+    for(int i=0;i<n;i++){
+        cout<< arr[i]<<" ";
+
+    }
+    cout<<endl;
+    return 0;
+}
